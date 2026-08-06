4949654d81212dfd2d73052e30977ed02a2f5e36
ShreeyaTandukar <tandukar.shreeya56@gmail.com>
2026-08-06 19:59:48 +0545

    added hero sectio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
@@ -7,7 +8,7 @@ const Home = () => {
       <NavBar />
 
       <div className="home">
-        <h1>Home Page</h1>
+        <Hero />
       </div>
     </>
   );
