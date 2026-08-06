26d138bfbe38a23efc22f452f460e13b888dea49
ShreeyaTandukar <tandukar.shreeya56@gmail.com>
2026-08-06 18:12:35 +0545

    updated navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,52 @@
-<div className="header">
-  <div className="headerList">
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/wall">Wall</Link></li>
-      <li><Link to="/write">Write</Link></li>
-      <li><Link to="/badges">Badges</Link></li>
-    </ul>
+import logo from "../assets/Images/logo.png";
 
-    <div className="loginDropDown">
-      <button
-        className="login-btn"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        Login ▼
-      </button>
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-      {isOpen && (
-        <div className="dropdownMenu">
-          <Link to="/user-login">User Login</Link>
-          <Link to="/admin-login">Admin Login</Link>
+  return (
+    <div className="header">
+      <div className="headerList">
+        <div className="logo">
+          <img src={logo} alt="Confidia Logo" />        </div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+          <li>
+            <Link to="/wall">Wall</Link>
+          </li>
+
+          <li>
+            <Link to="/write">Write</Link>
+          </li>
+
+          <li>
+            <Link to="/badges">Badges</Link>
+          </li>
+        </ul>
+
+        <div className="loginDropDown">
+          <button
+            className="login-btn"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            Login
+          </button>
+
+          {isOpen && (
+            <div className="dropdownMenu">
+              <Link to="/user-login">User Login</Link>
+              <Link to="/admin-login">Admin Login</Link>
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </div>
+  );
+};
 
-  </div>
-</div>
+export default NavBar;
