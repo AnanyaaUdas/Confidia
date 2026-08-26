afbc4094301232144a0ff899a285edb05569a022
ShreeyaTandukar <tandukar.shreeya56@gmail.com>
2026-08-26 10:23:55 +0545

    added admin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./style/global.css";
-import "./style/Wall.css"
+import "./style/Wall.css";
 import "./style/profile.css";
 import "./style/MainWall.css";
-import "./style/Notification.css"
-import "./style/AdminLogin.css"
+import "./style/Notification.css";
+import "./style/AdminLogin.css";
 import "./style/Auth.css";
 
 import Home from "./pages/Home";
@@ -15,25 +16,68 @@ import AdminLogin from "./pages/AdminLogin";
 import Auth from "./pages/Auth";
 import BadgeCelebration from "./components/BadgeCelebration";
 
+import Admin from "./components/Admin";
+import AdminDashboard from "./components/AdminDashboard";
 
 function App() {
-  return (
-    <>
-   
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/wall" element={<Wall />} />
-        <Route path="/write" element={<Write />} />
-        <Route path="/Profile" element={<Profile />} />
-        <Route path="/AdminLogin" element={<AdminLogin />} />
-        <Route path="/Auth" element={<Auth />} />
-      </Routes>
+    return (
+        <BrowserRouter>
+            <Routes>
 
-      <BadgeCelebration />
-    </BrowserRouter>
-    </>
-  );
+                {/* =========================
+                    USER PAGES
+                ========================= */}
+
+                <Route
+                    path="/"
+                    element={<Home />}
+                />
+
+                <Route
+                    path="/wall"
+                    element={<Wall />}
+                />
+
+                <Route
+                    path="/write"
+                    element={<Write />}
+                />
+
+                <Route
+                    path="/Profile"
+                    element={<Profile />}
+                />
+
+                <Route
+                    path="/Auth"
+                    element={<Auth />}
+                />
+
+                {/* =========================
+                    ADMIN PAGES
+                ========================= */}
+
+                <Route
+                    path="/AdminLogin"
+                    element={<AdminLogin />}
+                />
+
+                <Route
+                    path="/admin"
+                    element={<Admin />}
+                />
+
+                <Route
+                    path="/admin/dashboard"
+                    element={<AdminDashboard />}
+                />
+
+            </Routes>
+
+            <BadgeCelebration />
+
+        </BrowserRouter>
+    );
 }
 
 export default App;
